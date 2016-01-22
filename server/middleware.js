3aa0bc0360a767d151138492474b9a5d7c6403ac
Chris Rinaldi <cgrinaldi@gmail.com>
2016-01-22 15:03:52 -0800

    Create skeleton for /api/users

--- server/middleware.js
+++ server/middleware.js
@@ -1,6 +1,16 @@
+var express = require('express');
 var morgan = require('morgan');
 
 module.exports = function(app) {
   // Add logging of server requests/responses
   app.use(morgan('dev'));
+
+  // Create Express routers
+  var usersRouter = express.Router();
+
+  // Use users router for requests related to adding and getting users
+  app.use('/api/users', usersRouter);
+
+  // Setup all of the routers
+  require('./users/userRoutes.js')(usersRouter);
 };
---
+++ server/users/usersController.js
@@ -0,0 +1,9 @@
+module.exports = {
+  getUsers (req, res) {
+    res.send('Getting users!');
+  },
+
+  addUser (req, res) {
+    res.send('Adding user!');
+  }
+}
